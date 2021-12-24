97bb564264e2f7a2e74811c0cde3f94af8b99811
Bas Boot <b.boot@student.tudelft.nl>
2021-12-24 11:03:50 +0100

    Add timing

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ t = 9999999996
 z = 668982906662259241898499065103473245960164961956379608176477385568916730412708940627183786580130305413916213903107370821687807114455622233129065284523542286660747383208859057005014109901179025180585657310560580373893562272798582372082139636432607739424027324558809706621124173160967213211956896669859138626558962483744668579313732058599658824157337817420528373805887095539165385293563750768
 
 import time
+import timeit
 import pickle
 import os.path
 
@@ -20,12 +21,12 @@ z = 0x13 # (hex)
 
 # compute 2^(2^t) (mod n)
 
-LOGGING_FREQUENCY = 2   # log intermediate result every # iterations
+LOGGING_FREQUENCY = 6   # log intermediate result every # iterations
 PERSISTENT_DATA_FILE = "rivest.pkl"
 
 
-def save_data(result, i):
-    data = {"result": result, "i": i}
+def save_data(result, i, total_time):
+    data = {"result": result, "i": i, "total_time": total_time}
     file = open(PERSISTENT_DATA_FILE, 'wb')
     pickle.dump(data, file)
     file.close()
@@ -35,14 +36,18 @@ def load_data():
     file = open(PERSISTENT_DATA_FILE, 'rb')
     data = pickle.load(file)
     file.close()
-    return data["result"], data["i"]
+    return data["result"], data["i"], data["total_time"]
 
 # Create data file if not exists
 if not os.path.isfile(PERSISTENT_DATA_FILE):
     # initial data
-    save_data(2, 0)
+    save_data(2, 0, 0)
 
-result, start_i = load_data()
+# Load data to (re)start
+result, start_i, time_offset = load_data()
+
+# Current start time
+start_time = timeit.default_timer()
 
 logging_count = 0
 for i in range(start_i, t):
@@ -50,13 +55,17 @@ for i in range(start_i, t):
 
     time.sleep(1)
 
+    # Log and save data
     logging_count += 1 # addition is less expensive than modulo
-    if logging_count == LOGGING_FREQUENCY:
+    if logging_count == LOGGING_FREQUENCY or i == t - 1:
         logging_count = 0
         print(f"-----------------------------------------------------")
         print(f"t = {i+1}, intermediate result = {result}")
-        print(f"progress = {int((i/t)*10000)/100}%")
-        save_data(result, i+1) # increase i, to restart at next iteration with current result
+        stop_time = timeit.default_timer()
+        total_time = time_offset + stop_time - start_time
+        print(f"progress = {i / t * 100: .2f}%, current running time = {stop_time - start_time: .2f}s, total time = {total_time: .2f}s")
+
+        save_data(result, i+1, total_time) # increase i, to restart at next iteration with current result
 
 print(f"2^(2^{t}) = {result}")
 print(f"{hex(z)} (z) XOR {result} = {hex(z ^ result)}")
